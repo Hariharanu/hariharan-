f595151bd1bd9cb46d8f8b7803ba3bc531b01395
hariharan <31499070+Hariharanu@users.noreply.github.com>
2017-09-10 12:17:53 +0530

    Create biggest.cpp

---
+++ biggest.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+void manin ()
+{
+int a,b,c;
+cin>> a>>b>>c;
+if(a>=b&&a>=c)
+cout<<"a is biggest number ";
+if(b>=a&&b>=c)
+cout<<"b is biggest number ";
+if(c>=a&&c>=b)
+cout<<"c is biggest number";
+}
